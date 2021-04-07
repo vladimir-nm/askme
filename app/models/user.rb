8057fd52bcba57dddc90d1ae2ba365daa19a9492
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-04-07 21:37:49 +0300

    version 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_170446) do
+ActiveRecord::Schema.define(version: 2021_04_06_184517) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2021_04_03_170446) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "avatar_url"
   end
 
 end
