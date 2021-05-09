c62f1e8dba0052f75bf255d44ca0847279237e42
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-09 16:58:38 +0300

    Adding a number of questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  # Это действие отзывается, когда пользователь заходит по адресу /users
   def index
-    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
-    # модели мы просто вызываем метод User.new, который создает модель, не
-    # записывая её в базу.
     @users = [
       User.new(
         id: 1,
@@ -12,8 +8,12 @@ class UsersController < ApplicationController
         avatar_url: 'https://secure.gravatar.com/avatar/' \
           '71269686e0f757ddb4f73614f43ae445?s=100'
       ),
-      User.new(id: 2, name: 'Vladimir', username: 'Vladimir')
-    ]
+      User.new(
+        id: 2,
+        name: 'Vladimir',
+        username: 'Vladimir'
+      )
+     ]
   end
 
   def new
@@ -22,28 +22,18 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # Это действие отзывается, когда пользователь заходит по адресу /users/:id,
-  # например /users/1.
   def show
-    # Болванка пользователя
     @user = User.new(
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/' \
-        '71269686e0f757ddb4f73614f43ae445?s=100'
+      name: 'Vladimir',
+      username: 'Vladimir'
     )
 
-    # Болванка вопросов для пользователя
     @questions = [
       Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(
-        text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
-      )
+      Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Тестовый вопрос', created_at: Date.parse('27.03.2016'))
     ]
 
-    # Болванка для нового вопроса
     @new_question = Question.new
-
-    # Обратите внимание, пока ни одна из болванок не достается из базы
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def inclination(number_of_questions)
+    questions = %w[вопрос вопроса вопросов]
+
+    return questions.last if (11..14).include?(number_of_questions % 100)
+
+    ostatok = number_of_questions % 10
+
+    case ostatok
+    when 1
+      questions.first
+    when 2..4
+      questions[1]
+    else
+      questions.last
+    end
+  end
 end
