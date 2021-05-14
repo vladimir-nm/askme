102ee96c9c8d07b8a8c6d5fdf227e74f49256cca
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-14 15:50:53 +0300

    add update user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: %i[index create new]
+
   def index
     @users = User.all
   end
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      # redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
       flash[:success] = 'Пользователь успешно зарегистрирован!'
       redirect_to root_url
     else
@@ -20,11 +22,20 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
   end
-  
+
+  def update
+    if @user.update(user_params)
+      # redirect_to user_path(@user), notice: 'Данные обновлены!'
+      flash[:success] = 'Данные обновлены!'
+      redirect_to user_path(@user)
+    else
+      flash[:error] = 'Something went wrong'
+      render :edit
+    end
+  end
+
   def show
-    @user = User.find params[:id]
     @questions = @user.questions.order(create_at: :desc)
 
     @new_question = @questions.build
@@ -32,8 +43,11 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation,:name, :username, :avatar_url)
+  def load_user
+    @user ||= User.find params[:id]
   end
 
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
+  end
 end
