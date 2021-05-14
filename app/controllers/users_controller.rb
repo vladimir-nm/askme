7a3127a50c541acbc6df15bd0d57654e87f2a8c0
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-14 07:07:00 +0300

    add flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Object successfully created'
-      redirect_to @user
+      #redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      flash[:success] = 'Пользователь успешно зарегистрирован!'
+      redirect_to root_url
     else
       flash[:error] = 'Something went wrong'
       render :new
@@ -19,10 +20,14 @@ class UsersController < ApplicationController
   end
 
   def edit
+    
   end
-
+  
   def show
+    @user = User.find params[:id]
+    @questions = @user.questions.order(create_at: :desc)
 
+    @new_question = @questions.build
   end
 
   private
