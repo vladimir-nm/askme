05c6239b0b07c9de2b726809709f9836100c0d71
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-15 11:58:40 +0300

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destory]
   resources :questions
-
-  # get 'users/index'
-  # get 'users/new'
-  # get 'users/edit'
-  # get 'users/show'
   
   get 'show' => 'users#show'
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
