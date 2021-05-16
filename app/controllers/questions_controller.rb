4b7c5673b39feda3f31b57ebefe8a69f7c141358
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-16 10:58:38 +0300

    security fix user_id

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,8 +43,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
-    # он может менять ответы на вопрос, ему доступно также поле :answer.
     if current_user.present? &&
        params[:question][:user_id].to_i == current_user.id
       params.require(:question).permit(:user_id, :text, :answer)
