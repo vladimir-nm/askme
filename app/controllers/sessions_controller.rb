fd4ad5bacd875da50c156e25fef96b4c341f2a4f
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-18 22:37:41 +0300

    change image avatar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.authenticate(params[:email], params[:password])
-    if @user.present?
-      session[:user_id] = @user.id 
+    user = User.authenticate(params[:email], params[:password])
+    if user.present?
+      session[:user_id] = user.id 
       redirect_to root_url, notice: 'Вы успешно залогинились!'     
     else
       flash.now.alert = 'Неправильный email или пароль'
